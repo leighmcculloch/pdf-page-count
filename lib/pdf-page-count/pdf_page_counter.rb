81661a0ff0ff084c8f2b6ea29fb3b38a02cbcd74
Leigh McCulloch <leigh@mcchouse.com>
2014-02-15 16:31:36 +1100

    Expanded interface so it will be easier to add parallism.

--- lib/pdf-page-count/pdf_page_counter.rb
+++ lib/pdf-page-count/pdf_page_counter.rb
@@ -8,10 +8,16 @@ module PdfPageCount
       @total_pages = 0
     end
     
+    def start_counting
+    end
+    
     def add_pdf_file(file)
       pages = PdfPageCount::PdfUtil.count_pages(file)
       @total_pages += pages
       yield file, pages
+    end
+    
+    def finish_counting
     end
     
     def total_pages
