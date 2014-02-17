b52c912fcfca0f13c15d0d6a5e0d5edcd054c560
Leigh McCulloch <leigh@mcchouse.com>
2014-02-18 00:00:31 +1100

    Moved more logic from the executable script into the library.

--- lib/pdf-page-count/pdf_page_counter.rb
+++ lib/pdf-page-count/pdf_page_counter.rb
@@ -11,8 +11,25 @@ module PdfPageCount
     def start_counting
     end
     
+    def add_pdf_paths(paths, recursive: false)
+      paths.each do |path|
+        self.add_pdf_path(path, recursive: recursive) do |file, pages|
+          yield file, pages
+        end
+      end
+    end
+    
+    def add_pdf_path(path, recursive: false)
+      files = FileUtil.find_pdf_files(path, recursive: recursive)
+      files.each do |file|
+        self.add_pdf_file(file) do |file, pages|
+          yield file, pages
+        end
+      end
+    end
+    
     def add_pdf_file(file)
-      pages = PdfPageCount::PdfUtil.count_pages(file)
+      pages = PdfUtil.count_pages(file)
       @total_pages += pages
       yield file, pages
     end
